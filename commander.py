3fe7325eea30751c768746f7d77f7c2e8622999f
Corban Brantley <brantlc4@gmail.com>
2020-08-12 22:07:09 -0700

    Define get_filters function

--- commander.py
+++ commander.py
@@ -1,2 +1,19 @@
 import pandas as pd
 import numpy as np
+
+def get_filters():
+    """
+    Asks user to specify if they want to see stats about winners or losers.
+
+    :return: 
+        (str) placing - place of player in final standings
+    """
+
+    print('Hello! Let\'s explore some Magic the Gathering Commander data!')
+    placing = input('Do you want to see stats about winners or losers?\n').lower()
+
+    while placing != 'winners' and placing != 'losers':
+        placing = input('Please type winners or losers.\n').lower()
+
+    print('-'*40)
+    return placing
