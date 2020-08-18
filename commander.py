fc82164a940f507e02615fd2368da26a78a2ec16
Corban Brantley <brantlc4@gmail.com>
2020-08-17 19:45:06 -0700

    feat: add more statistics to color_stats(df)

--- commander.py
+++ commander.py
@@ -5,28 +5,60 @@ import numpy as np
 def color_stats(df):
     """Displays statistics about each color and color combination."""
 
-    print('\nCalculating the win percentage of each color and color combination...\n')
-    start_time = time.time()
-    colors = ['W', 'U', 'B', 'R', 'G', 'WU', 'WB', 'WR', 'WG', 'UB', 'UR', 'UG', 'BR',
-              'BG', 'RG', 'WUB', 'WUR', 'WUG', 'WBR', 'WBG', 'WRG', 'UBR', 'UBG', 'URG',
-              'BRG', 'WUBR', 'WUBG', 'WURG', 'WBRG', 'UBRG', 'C']
+    check = input('\nWould you like to see statistics based on color? y or n?\n').lower()
+    while check != 'y' and check != 'n':
+        check = input('\nPlease type y or n.\n').lower()
+    if check == 'y':
+        start_time = time.time()
+        colors = ['W', 'U', 'B', 'R', 'G', 'WU', 'WB', 'WR', 'WG', 'UB', 'UR', 'UG', 'BR',
+                  'BG', 'RG', 'WUB', 'WUR', 'WUG', 'WBR', 'WBG', 'WRG', 'UBR', 'UBG', 'URG',
+                  'BRG', 'WUBR', 'WUBG', 'WURG', 'WBRG', 'UBRG', 'WUBRG', '0']
 
-    for color in colors:
-        df = pd.read_csv('commander_data.csv')
-        df = df[df['Color Identity of Commander'] == color]
-        if df.empty:
-            print('There is no data for {}.'.format(color))
-        else:
-            wins = 0
-            for record in df['Player Won']:
-                if record == 'Yes':
-                    wins += 1
-            total_games = df['Player Won'].count()
-            win_percentage = round(wins/total_games * 100)
-            print('{}: {}%'.format(color, win_percentage))
+        one_color = []
+        two_color = []
+        three_color = []
+        four_color = []
+        five_color = []
 
-    print("\nThis took %s seconds." % (time.time() - start_time))
-    print('-' * 40)
+        for color in colors:
+            df = pd.read_csv('commander_data.csv')
+            df = df[df['Color Identity of Commander'] == color]
+
+            if df.empty:
+                print('There is no data for {}.'.format(color))
+            else:
+                wins = 0
+                for record in df['Player Won']:
+                    if record == 'Yes':
+                        wins += 1
+                total_games = df['Player Won'].count()
+                win_percentage = round(wins/total_games * 100)
+                print(color + '\nWin percentage: ' + str(win_percentage) + '%\nMost common deck archetype: ' +
+                      df['Deck Type'].mode()[0] + '\nMost common commander: ' + df['Commander'].mode()[0] + '\n')
+
+                if len(color) == 1:
+                    one_color.append(win_percentage)
+                elif len(color) == 2:
+                    two_color.append(win_percentage)
+                elif len(color) == 3:
+                    three_color.append(win_percentage)
+                elif len(color) == 4:
+                    four_color.append(win_percentage)
+                elif len(color) == 5:
+                    five_color.append(win_percentage)
+
+        def Average(lst):
+            return sum(lst) / len(lst)
+
+        print('Win percentage by number of colors:\nOne: ' + str(round(Average(one_color))) + '\nTwo: ' +
+              str(round(Average(two_color))) + '\nThree: ' + str(round(Average(three_color))) + '\nFour: ' +
+              str(round(Average(four_color))) + '\nFive: ' + str(round(Average(five_color))) + '\nColorless: ' +
+              str(win_percentage))
+
+
+
+        print("\nThis took %s seconds." % (time.time() - start_time))
+        print('-' * 40)
 
 def main():
     while True:
