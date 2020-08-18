651b2a9b1042518d5939156f67be8a7a73bf978f
Corban Brantley <brantlc4@gmail.com>
2020-08-18 16:30:47 -0700

    feat: add test for statistical significance

--- commander.py
+++ commander.py
@@ -1,6 +1,7 @@
 import time
 import pandas as pd
 import numpy as np
+import scipy.stats as ss
 
 def color_stats(df):
     """Displays statistics about each color and color combination."""
@@ -36,6 +37,14 @@ def color_stats(df):
         red_games = []
         green_games = []
 
+        def binom_prob(games, successes):
+            hh = ss.binom(games, .25)
+
+            p = 0
+            for k in range(1, successes + 1):
+                p += hh.pmf(k)
+            return p
+
         for color in colors:
             df = pd.read_csv('commander_data.csv')
             df = df[df['Color Identity of Commander'] == color]
@@ -48,9 +57,19 @@ def color_stats(df):
                     if record == 'Yes':
                         wins += 1
                 total_games = df['Player Won'].count()
-                win_percentage = round(wins/total_games * 100)
-                print(color + '\nWin percentage: ' + str(win_percentage) + '%\nMost common deck archetype: ' +
-                      df['Deck Type'].mode()[0] + '\nMost common commander: ' + df['Commander'].mode()[0] + '\n')
+                win_percentage = round(wins/total_games * 100, 1)
+
+                #Test for statistcal significance.
+                total_p = binom_prob(total_games, wins)
+
+                if round(total_p) < .05:
+                    print(color, '\nWin percentage: ', win_percentage, '%\nMost common deck archetype: ',
+                          df['Deck Type'].mode()[0], '\nMost common commander: ', df['Commander'].mode()[0],
+                          '\nThis win rate is statistcally significant.\n')
+                else:
+                    print(color, '\nMost common deck archetype: ', df['Deck Type'].mode()[0],
+                          '\nMost common commander: ', df['Commander'].mode()[0],
+                          '\nNo statistically significant win rate data for this color combination.\n')
 
                 if color != '0' and len(color) == 1:
                     one_color_wins.append(wins)
@@ -72,32 +91,45 @@ def color_stats(df):
                 if 'W' in color:
                     white_wins.append(wins)
                     white_games.append(total_games)
-                elif 'U' in color:
+                if 'U' in color:
                     blue_wins.append(wins)
                     blue_games.append(total_games)
-                elif 'B' in color:
+                if 'B' in color:
                     black_wins.append(wins)
                     black_games.append(total_games)
-                elif 'R' in color:
+                if 'R' in color:
                     red_wins.append(wins)
                     red_games.append(total_games)
-                elif 'G' in color:
+                if 'G' in color:
                     green_wins.append(wins)
                     green_games.append(total_games)
 
+        one_color_p = binom_prob(sum(one_color_games), sum(one_color_wins))
+        two_color_p = binom_prob(sum(two_color_games), sum(two_color_wins))
+        three_color_p = binom_prob(sum(three_color_games), sum(three_color_wins))
+        four_color_p = binom_prob(sum(four_color_games), sum(four_color_wins))
+        five_color_p = binom_prob(sum(five_color_games), sum(five_color_wins))
 
-        print('Win percentage by number of colors:\nOne: ' + str(round(sum(one_color_wins) / sum(one_color_games) *
-              100)) + '%\nTwo: ' + str(round(sum(two_color_wins) / sum(two_color_games) * 100)) + '%\nThree: ' +
-              str(round(sum(three_color_wins) / sum(three_color_games) * 100)) + '%\nFour: ' +
-              str(round(sum(four_color_wins) / sum(four_color_games) * 100)) + '%\nFive: ' +
-              str(round(sum(five_color_wins) / sum(five_color_games) * 100)) + '%\nColorless: ' + str(win_percentage) +
-              '%')
+        white_p = binom_prob(sum(white_games), sum(white_wins))
+        blue_p = binom_prob(sum(blue_games), sum(blue_wins))
+        black_p = binom_prob(sum(black_games), sum(black_wins))
+        red_p = binom_prob(sum(red_games), sum(red_wins))
+        green_p = binom_prob(sum(green_games), sum(green_wins))
 
-        print('\nWin percentage by color (mono or in a combination):\nWhite: ' + str(round(sum(white_wins) / sum(white_games) *
-              100)) + '%\nBlue: ' + str(round(sum(blue_wins) / sum(blue_games) * 100)) + '%\nBlack: ' +
-              str(round(sum(black_wins) / sum(black_games) * 100)) + '%\nRed: ' + str(round(sum(red_wins) /
-              sum(red_games) * 100)) + '%\nGreen: ' + str(round(sum(green_wins) / sum(green_games) * 100)) +
-              '%\nColorless: ' + str(win_percentage) + '%')
+        print('Win percentage by number of colors:\nOne:', round(sum(one_color_wins) / sum(one_color_games) * 100, 1),
+              '% p =', round(one_color_p, 3), '\nTwo:', round(sum(two_color_wins) / sum(two_color_games) * 100, 1),
+              '% p =', round(two_color_p, 3), '\nThree:', round(sum(three_color_wins) / sum(three_color_games) * 100,
+              1), '% p =', round(three_color_p, 3), '\nFour:', round(sum(four_color_wins) / sum(four_color_games)
+              * 100, 1), '% p =', round(four_color_p, 3), '\nFive:', round(sum(five_color_wins)
+              / sum(five_color_games) * 100, 1), '% p =', round(five_color_p, 3), '\nColorless:', win_percentage,
+              '% p =', round(total_p, 3))
+
+        print('\nWin percentage by color (mono or in a combination):\nWhite:', round(sum(white_wins) / sum(white_games)
+              * 100, 1), '% p =', round(white_p, 3), '\nBlue:', round(sum(blue_wins) / sum(blue_games) * 100, 1),
+              '% p =', round(blue_p, 3), '\nBlack:', round(sum(black_wins) / sum(black_games) * 100, 1), '% p =',
+              round(black_p, 3), '\nRed:', round(sum(red_wins) / sum(red_games) * 100, 1), '% p =', round(red_p, 3),
+              '\nGreen:', round(sum(green_wins) / sum(green_games) * 100, 1), '% p =', round(green_p, 3),
+              '\nColorless:', win_percentage, '% p =', round(total_p, 3)) 
 
 
         print("\nThis took %s seconds." % (time.time() - start_time))
